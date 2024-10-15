3e417f994fd6fc233a2507de29bf4f5140375a63
Dhruv-Varshney-Developer <f20200865@pilani.bits-pilani.ac.in>
2024-10-15 09:55:33 +0530

    connectwallet and cards for oracles added.

---
+++ frontend/multi-oracle-defi/src/screens/PriceFeed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PriceFeed = () => {
+  return (
+    <div>
+      <h1>Price Feed Oracle</h1>
+      {/* Add your price feed oracle content here */}
+    </div>
+  );
+};
+
+export default PriceFeed;
