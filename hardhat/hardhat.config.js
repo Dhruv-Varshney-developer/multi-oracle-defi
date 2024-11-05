3d1742a054d4919f9f9cebd784aa082a0d85ee8e
Dhruv-Varshney-Developer <f20200865@pilani.bits-pilani.ac.in>
2024-11-05 22:44:06 +0530

    hardhat config modified to use sepolia.

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,39 +2,45 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomicfoundation/hardhat-verify");
 
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
-  settings:{
-    optimizer:{
+  settings: {
+    optimizer: {
       enabled: true,
       runs: 200,
-    }
+    },
   },
   networks: {
     polygonAmoy: {
       url: process.env.RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
 
-      chainId: 80002
+      chainId: 80002,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111,
     },
   },
 
   etherscan: {
     apiKey: {
-      polygonAmoy: process.env.POLYGONSCAN_API_KEY
-    }},
+      polygonAmoy: process.env.POLYGONSCAN_API_KEY,
+      sepolia: process.env.ETHERSCAN_API_KEY,
+    },
+  },
 
-
-  okxweb3explorer:{
+  okxweb3explorer: {
     apiKey: process.env.OK_LINK_API,
     customChains: [
       {
         network: "Amoy Testnet",
         chainId: 80002,
         urls: {
-          apiURL: "https://www.oklink.com/api/v5/explorer/contract/verify-source-code-plugin/AMOY_TESTNET",
+          apiURL:
+            "https://www.oklink.com/api/v5/explorer/contract/verify-source-code-plugin/AMOY_TESTNET",
           browserURL: "https://www.oklink.com",
         },
       },
