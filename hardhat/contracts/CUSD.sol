52238137a92dfeed0400f6d8167bbdbdaf5a8eb8
Dhruv-Varshney-Developer <f20200865@pilani.bits-pilani.ac.in>
2024-11-08 04:23:26 +0530

    _update and _ approve overridden and documentation added.

--- hardhat/contracts/CUSD.sol
+++ hardhat/contracts/CUSD.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+// The smallest unit for this token is cent.
 
 pragma solidity ^0.8.22;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -7,10 +8,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CapstoneUSD is ERC20, Ownable {
     AggregatorV3Interface public priceFeed;
+    address public mintAddress;
 
     constructor() ERC20("CapstoneUSD", "CUSD") Ownable(msg.sender) {
         // Mint some initial tokens for the contract's liquidity pool
-        _mint(address(this), 1000000); // 1 million CUSD tokens
+        _mint(msg.sender, 1000000); // 1 million CUSD tokens (in smallest units ) minted to deployer.
+        mintAddress = msg.sender;
 
         //  Chainlink price feed for ETH/USD on Sepolia testnet
         priceFeed = AggregatorV3Interface(
@@ -20,7 +23,45 @@ contract CapstoneUSD is ERC20, Ownable {
 
     // Mint function to mint new tokens (for simplicity)
     function mint(address to, uint256 amount) external {
+        require(msg.sender == mintAddress, "Not allowed to mint");
+
         _mint(to, amount);
+    }
+
+    function setMintAddress(address _mintAddress) external onlyOwner {
+        mintAddress = _mintAddress;
+    }
+
+    /* 
+@note ERC20 from openzeppelin mints tokens in smallest unit. We call it cents for this token. 
+To make sure, whenever any of mint, burn, transfer, approve, etc 
+is called we are actually minting tokens in CUSD and not cents, _update and _approve has been overridden.
+
+@Example 
+
+Let's say we call _mint function.
+
+If these functions were not overridden, _mint(to, 1000000) would mint 0.0000000000001 CUSD
+Since these functions are now overridden, _mint(to, 1000000) will now mint 1000000 CUSD.
+
+
+*/
+
+    function _update(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        super._update(from, to, amount * 10 ** 18);
+    }
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 value,
+        bool emitEvent
+    ) internal override {
+        super._approve(owner, spender, value * 10 ** 18, emitEvent);
     }
 
     // Function to buy CUSD tokens using ETH
