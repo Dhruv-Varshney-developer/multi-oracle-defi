486f4a895d5b60e728015b9a785442fffde44cfb
Dhruv-Varshney-Developer <f20200865@pilani.bits-pilani.ac.in>
2024-11-07 23:57:44 +0530

    added sepolia in web3 provider.

--- frontend/multi-oracle-defi/src/utils/web3provider.js
+++ frontend/multi-oracle-defi/src/utils/web3provider.js
@@ -1,16 +1,15 @@
 import { WagmiProvider, createConfig, http } from "wagmi";
-import {   polygonAmoy } from "wagmi/chains";
+import { polygonAmoy, sepolia } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import React from "react";
 
 export const config = createConfig(
   getDefaultConfig({
-    chains: [ polygonAmoy],
+    chains: [polygonAmoy, sepolia],
     transports: {
-      
       [polygonAmoy.id]: http(`https://rpc-amoy.polygon.technology`),
-      
+      [sepolia.id]: http(`https://rpc.sepolia.org`),
     },
     walletConnectProjectId: "af699c419e11d1dbc5cb8c53fb279dee",
     appName: "Oracle",
