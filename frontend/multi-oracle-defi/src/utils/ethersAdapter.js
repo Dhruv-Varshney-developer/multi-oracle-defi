4ea85cf88ea0571e4132f6fa0c524873f3c9624b
Dhruv-Varshney-Developer <f20200865@pilani.bits-pilani.ac.in>
2024-11-14 09:43:30 +0530

    Fix ethersAdapter.js to have a signer.

--- frontend/multi-oracle-defi/src/utils/ethersAdapter.js
+++ frontend/multi-oracle-defi/src/utils/ethersAdapter.js
@@ -1,27 +1,30 @@
 import { providers } from "ethers";
 import { useMemo } from "react";
-import { useClient } from "wagmi";
+import { Config, useConnectorClient } from "wagmi";
 
-export function clientToProvider(client) {
-  const { chain, transport } = client;
+/**
+ * Converts a Viem Client to an ethers.js Signer.
+ * @param {Client} client - The Viem client containing the account, chain, and transport information.
+ * @returns {Signer} - The ethers.js Signer instance.
+ */
+export function clientToSigner(client) {
+  const { account, chain, transport } = client;
   const network = {
     chainId: chain.id,
     name: chain.name,
     ensAddress: chain.contracts?.ensRegistry?.address,
   };
-  if (transport.type === "fallback")
-    return new providers.FallbackProvider(
-      transport.transports.map(
-        ({ value }) => new providers.JsonRpcProvider(value?.url, network)
-      )
-    );
-  return new providers.JsonRpcProvider(transport.url, network);
+  const provider = new providers.Web3Provider(transport, network);
+  const signer = provider.getSigner(account.address);
+  return signer;
 }
 
-export function useEthersProvider({ chainId } = {}) {
-  const client = useClient({ chainId });
-  return useMemo(
-    () => (client ? clientToProvider(client) : undefined),
-    [client]
-  );
+/**
+ * React hook to convert a Viem Client to an ethers.js Signer.
+ * @param {object} options - Optional configuration options, including the chainId.
+ * @returns {Signer|undefined} - The ethers.js Signer instance if available, otherwise undefined.
+ */
+export function useEthersSigner({ chainId } = {}) {
+  const { data: client } = useConnectorClient({ chainId });
+  return useMemo(() => (client ? clientToSigner(client) : undefined), [client]);
 }
